f70837a6f93908632ad0e67dd13e301b01dd9d1b
igorcaptain <gmalashevich09@gmail.com>
2020-09-22 00:22:58 +0300

    Remove printing method

--- SmartTranslator/Program.cs
+++ SmartTranslator/Program.cs
@@ -41,36 +41,13 @@ namespace SmartTranslator
 
             stopwatch.Stop();
 
-            PrintData(data, TranslateStringState.TranslatedNormalized, stopwatch.ElapsedMilliseconds / 1000.0);
-
             Console.ReadLine();
 
-            
-            
             Console.WriteLine("Saving...");
             IExcelExporter<TranslateString> excelExporter = new ExcelExporter<TranslateString>();
             excelExporter.ExportData(data, fileName, "TranslatedString", "CellCode");
             Console.WriteLine("Done!");
             Console.ReadLine();
-        }
-
-        private static void PrintData(IList<TranslateString> data, TranslateStringState? state, double deltaTime)
-        {
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("PRINTING DATA");
-            int countOfTranslated = 0;
-            foreach (var item in data)
-            {
-                if (state != null && item.TranslateStringState != state)
-                    continue;
-                Console.WriteLine("---------------------------------------");
-                Console.WriteLine($"{item.CellCode} - {item.LoadedString}; \nnormalized: {item.NormalizedString}; \nreplaceToken: {item.ReplaceToken}; \ntranslate: {item.TranslatedString}");
-                Console.WriteLine("---------------------------------------");
-                countOfTranslated++;
-            }
-            Console.WriteLine($"Result: {countOfTranslated} translated string(s) for {deltaTime} sec...");
-            Console.WriteLine("END OF PRINTING");
-            Console.WriteLine();
         }
     }
 }
